c21626aa34e613c6110aad4fe5771e4f7ccd3af9
Robert Snyder <r.snyder49@gmail.com>
2018-11-08 21:15:10 +0000

    automatically backed up by learn

--- db/migrate/20181108211022_create_dogs.rb
+++ db/migrate/20181108211022_create_dogs.rb
@@ -1,13 +1,9 @@
 class CreateDogs < ActiveRecord::Migration
-  def up 
-    create_table :dogs do |t|
+  def change 
+    create_table :dogs do |t| 
       t.string :name 
       t.string :breed 
-    end
-  end 
-  
-  def down 
-    drop_table :dogs
-  end 
+    end 
+  end
   
 end
